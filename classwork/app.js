b95310702ccc8731da9a47fa370736408dfee3a7
kamranferoz <kamranferoz@yahoo.com>
2019-01-30 12:07:44 +0500

    Commit done

--- classwork/app.js
+++ classwork/app.js
@@ -1,8 +1,2 @@
-console.log("Hello!");
-name = "Mark";
-console.log(name);
-name2 = "Ace";
-console.log(name2);
-var num = "20";
-var total = 7 + num;
-console.log(total);
+alert ('Thanks for your input! ');
+
